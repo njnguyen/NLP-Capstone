8b168b56e259bee530ba127a3460739db4f85073
mingsh1 <101757228+mingsh1@users.noreply.github.com>
2024-05-25 16:19:54 -0700

    Update initialize_embeddings.sh

--- scripts/initialize_embeddings.sh
+++ scripts/initialize_embeddings.sh
@@ -7,35 +7,35 @@ TRAIN_DATA_PATH=$5
 
 source ${PATH_TO_CONDA}/miniconda3/bin/activate $VENVNAME
 
-# python src/reinitialize_embeddings.py \
-#     --old_model_path xlm-roberta-base \
-#     --new_vocab_file tokenizers/$NEW_VOCAB \
-#     --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script.pt \
-#     --reinit_by_script
+python src/reinitialize_embeddings.py \
+    --old_model_path xlm-roberta-base \
+    --new_vocab_file tokenizers/$NEW_VOCAB \
+    --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script.pt \
+    --reinit_by_script
 
-# python src/reinitialize_embeddings.py \
-#     --old_model_path xlm-roberta-base \
-#     --new_vocab_file tokenizers/$NEW_VOCAB \
-#     --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_identity.pt \
-#     --reinit_by_identity
+python src/reinitialize_embeddings.py \
+    --old_model_path xlm-roberta-base \
+    --new_vocab_file tokenizers/$NEW_VOCAB \
+    --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_identity.pt \
+    --reinit_by_identity
 
-# python src/reinitialize_embeddings.py \
-#     --old_model_path xlm-roberta-base \
-#     --new_vocab_file tokenizers/$NEW_VOCAB \
-#     --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+identity.pt \
-#     --reinit_by_script --reinit_by_identity
+python src/reinitialize_embeddings.py \
+    --old_model_path xlm-roberta-base \
+    --new_vocab_file tokenizers/$NEW_VOCAB \
+    --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+identity.pt \
+    --reinit_by_script --reinit_by_identity
 
-# python src/reinitialize_embeddings.py \
-#     --old_model_path xlm-roberta-base \
-#     --new_vocab_file tokenizers/$NEW_VOCAB \
-#     --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+position.pt \
-#     --reinit_by_script --reinit_by_position
+python src/reinitialize_embeddings.py \
+    --old_model_path xlm-roberta-base \
+    --new_vocab_file tokenizers/$NEW_VOCAB \
+    --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+position.pt \
+    --reinit_by_script --reinit_by_position
 
-# python src/reinitialize_embeddings.py \
-#     --old_model_path xlm-roberta-base \
-#     --new_vocab_file tokenizers/$NEW_VOCAB \
-#     --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+pos+ident.pt \
-#     --reinit_by_script --reinit_by_position --reinit_by_identity
+python src/reinitialize_embeddings.py \
+    --old_model_path xlm-roberta-base \
+    --new_vocab_file tokenizers/$NEW_VOCAB \
+    --embedding_output_path tokenizers/${NEW_EMB_PREFIX}_emb_32k_script+pos+ident.pt \
+    --reinit_by_script --reinit_by_position --reinit_by_identity
 
 python src/reinitialize_embeddings.py \
     --old_model_path xlm-roberta-base \
